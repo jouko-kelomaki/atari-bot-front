a7c48939117f78db2169d3c35aa7f136c68bfc9d
Jouko Kelomäki <jouko.kelomaki@gmail.com>
2021-05-08 12:59:34 +0300

    display board with HTML canvas, some WIP work with the lines on the board

---
+++ src/Board.tsx
@@ -0,0 +1,115 @@
+import React from 'react'
+import blackStone from './images/black.png'
+import whiteStone from './images/white.png'
+import bgImage from './images/shinkaya.jpg'
+
+type StateType = {
+    images: {
+        bgImage: HTMLImageElement
+        blackStone: HTMLImageElement
+        whiteStone: HTMLImageElement
+    }
+    dummyString: string
+}
+
+type PropType = {
+    propstr: string
+}
+
+type Point = {
+    x: number
+    y: number
+}
+
+const loadableImages = ["bgImage", "blackStone", "whiteStone"] as const
+
+class Board extends React.Component<PropType, StateType> {
+    canvasRef: React.RefObject<HTMLCanvasElement>
+    contextRef: CanvasRenderingContext2D | null
+    imageLoaded: { bgImage: boolean; blackStone: boolean; whiteStone: boolean }
+    IsComponentMounted: boolean
+
+    constructor(props: PropType) {
+        super(props)
+        this.canvasRef = React.createRef()
+        this.contextRef = null
+        this.IsComponentMounted = false
+
+        this.imageLoaded = {
+            bgImage: false,
+            blackStone: false,
+            whiteStone: false
+        }
+        
+        this.state = {
+            images: this.createImageElements(),
+            dummyString: "a",
+        }
+    }
+
+    createImageElements(): {[I in typeof loadableImages[number]]: HTMLImageElement} {
+        let loadHelper = (imageName: typeof loadableImages[number]) => () => { 
+            this.imageLoaded = {...this.imageLoaded, [imageName]: true}
+            this.IsComponentMounted && this.forceUpdate()
+
+            if(this.IsComponentMounted && this.imageLoaded.bgImage && this.imageLoaded.blackStone && this.imageLoaded.whiteStone) {
+                this.drawBoardUtil()
+            }
+        }
+
+        let bgImg = new Image()
+        bgImg.src = bgImage
+        bgImg.onload = loadHelper("bgImage")
+        
+        let blackImg = new Image()
+        blackImg.src = blackStone
+        blackImg.onload = loadHelper("blackStone")
+
+        let whiteImg = new Image()
+        whiteImg.src = whiteStone
+        whiteImg.onload = loadHelper("whiteStone")
+        return {
+            bgImage: bgImg,
+            blackStone: blackImg,
+            whiteStone: whiteImg
+        }
+    }
+
+    drawLineUtil(start: Point, end: Point){
+        let ctx = this.contextRef
+        if(ctx == null) {
+            return
+        }
+        ctx.beginPath()
+        ctx.moveTo(start.x, start.y)
+        ctx.lineTo(end.x, end.y)
+        ctx.stroke()
+    }
+
+    drawBoardUtil(){
+        if(this.contextRef == null) return
+        this.contextRef.drawImage(this.state.images.bgImage, 0, 0)
+
+    }
+  
+    componentDidMount() {
+        this.IsComponentMounted = true
+        const canvas = this.canvasRef.current
+        if (canvas === null) return
+        this.contextRef = canvas.getContext('2d') 
+    }
+
+    render() {
+        return (<canvas ref={this.canvasRef} width="1000" height="1000" {...this.props}/>)
+    } 
+}
+
+const boardIntersectionCoordinates = (boardWidthAndHeight: number, gridPadding: number, boardSize: number) => {
+    const interval = (boardWidthAndHeight - 2 * gridPadding) / (boardSize - 1)
+    
+
+}
+
+
+
+export default Board
